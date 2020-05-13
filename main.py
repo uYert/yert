f7266586f1ecb4d03e887fbb38a5e6b6309fbfa0
Mio Akiyama <61921875+Saphielle@users.noreply.github.com>
2020-05-13 22:48:37 +0200

    small placeholders

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+"""
+MIT License
+
+Copyright (c) 2020 - Sudosnok, AbstractUmbra, Saphielle-Akiyama
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+"""
+
 import discord
 import pathlib
+import config
+
 from discord.ext import commands
 
 class Bot(commands.Bot):
@@ -9,3 +34,6 @@ class Bot(commands.Bot):
         for file in pathlib.Path('extensions').glob('**/*.py'):
             *tree, _ = file.parts 
             self.load_extension('.'.join(tree) + '.' + file.stem)  # fstrings would be ugly there 
+            
+if __name__ == '__main__':
+    Bot(command_prefix='yoink ').run(config.DISCORD_TOKEN)

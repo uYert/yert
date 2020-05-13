ec407972cbf6e9d33e086e4c491a2ee9ea0a780b
sudosnok <subsonic96@gmail.com>
2020-05-13 23:08:34 +0100

    Added Events cog

---
+++ cogs/events.py
@@ -0,0 +1,68 @@
+"""
+MIT License
+
+Copyright (c) 2020 - Sudosnok, AbstractUmbra, Saphielle-Akiyama
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+""" 
+
+#==events.py==#
+#imports
+import traceback
+
+#from imports
+from datetime import datetime
+from discord import Message
+from discord.ext import commands
+
+class Events(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+	def fmt(self, dt: datetime):
+		return dt.strftime("%Y %b %d: %H:%M:%S:%f")
+
+
+	@commands.Cog.listener()
+	async def on_ready(self):
+		pass
+	
+	@commands.Cog.listener()
+	async def on_message(self, message: Message):
+		pass
+
+	@commands.Cog.listener()
+	async def on_command(self, ctx: commands.Context):
+		pass
+
+	@commands.Cog.listener()
+	async def on_command_error(self, ctx: commands.Context, error: Exception):
+		if hasattr(ctx.command, 'on_error'):
+			return
+		error = getattr(error, 'original', error)
+		if isinstance(error, commands.MissingPermissions):
+			pass
+
+
+	@commands.Cog.listener()
+	async def on_command_completion(self, ctx: commands.Context):
+		pass
+
+def setup(bot):
+	bot.add_cog(Events(bot))

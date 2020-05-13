99521eebbe744a17e3fd59fe9bb9d03ff5c62bf4
sudosnok <subsonic96@gmail.com>
2020-05-13 22:41:10 +0100

    removed comments

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -53,10 +53,9 @@ class Moderation(commands.Cog):
 	async def kick(self, ctx: commands.Context, target: Member, *, reason:Optional[str]) -> None:
 		"""Kicks the given target for a reason"""
 		reason = reason or self.DEF_REASON
-		await target.kick(reason=reason)  # might just let you write all of them, you're way better at explaining lol
-	#how are we going to explain shit like in the ban command? how can we say 'you can supply a days arg if you want, but it wont break if you dont'? like <required arg> [optional arg] ? it should be yeah, the help command should be showing them like that, ill let you try and explain default args then lmao oof this is gonna be hard lol, will it be better if we assume the user isnt completely incompetent and just shout gitgud if something goes wrong? lmfao, why not the user is a stupid cunt anyways lol, imma go loo, one sec
+		await target.kick(reason=reason)
 	
-	@commands.command() # how about saying that it's an optionnal arg and define all of those when the user uses the help command
+	@commands.command() 
 	@commands.has_permissions(ban_members=True)
 	@commands.bot_has_permissions(ban_members=True)
 	async def unban(self, ctx: commands.Context, target: int, *, reason: Optional[str]) -> None:
@@ -82,33 +81,3 @@ class Moderation(commands.Cog):
 def setup(bot):
 	bot.add_cog(Moderation(bot))
  
-"""
-Hey
-You should be able to write too
-Snek best 
-ayyyyyyyyy
-this is noice
-do you have any ideas about what kind of game we should be amking 
-don't ignore me :c
-no u
-smh
-umbra is a person lmao
-haha yes 
-bet he is human 
-imagine being human lmao
-ikr
-fuck humans
-they suck
-like wtf 
-you have elfs and stuff lol 
-imagine only living to about 80 years old
-ikr
-barely enough, can't even ejoy yourself 
-holy typos lmao
-lmfao
-shit happen
-also, I'm just gonnaedit a little thing in your cog
-because pep 8 is gonna geet mad at me again otherwisr
-sure, good catch
-:thonk:
-"""

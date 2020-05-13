4af66f35b7bfab78e4ce4842bf26d1c67a9c59c1
Revan <AlexNorgaard1170@gmail.com>
2020-05-14 00:27:55 +0100

    moderation: reoredered imports and added some docstrings

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -20,64 +20,61 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-""" 
-#==moderation.py==#
-#imports
+"""
+from typing import Union, Optional
 
-#from imports
 from discord import Member, Object
 from discord.ext import commands
-from typing import Union, Optional
 
 
 class Moderation(commands.Cog):
-	def __init__(self, bot) -> None:
-		self.bot = bot
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
 
-		self.DEF_DAYS = 7
-		self.DEF_REASON = "No reason given"
+        self.DEF_DAYS = 7
+        self.DEF_REASON = "No reason given"
 
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    @commands.bot_has_permissions(ban_members=True)
+    async def ban(self, ctx: commands.Context, target: Union[Member, Object], days: Optional[int], *, reason: Optional[str]) -> None:
+        """Bans the given <target> for [reason], deleting [days] of messages"""  # that good?
+        days = days or self.DEF_DAYS
+        reason = reason or self.DEF_REASON
+        await ctx.guild.ban(target, delete_message_days=days, reason=reason)
 
-	@commands.command()
-	@commands.has_permissions(ban_members=True)  
-	@commands.bot_has_permissions(ban_members=True)
-	async def ban(self, ctx: commands.Context, target: Union[Member, Object], days: Optional[int], *, reason: Optional[str]) -> None:
-		"""Bans the given <target> for [reason], deleting [days] of messages"""  #that good?
-		days = days or self.DEF_DAYS
-		reason = reason or self.DEF_REASON
-		await ctx.guild.ban(target, delete_message_days=days, reason=reason)
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    @commands.bot_has_permissions(kick_members=True)
+    async def kick(self, ctx: commands.Context, target: Member, *, reason: Optional[str]) -> None:
+        """Kicks the given target for a reason"""
+        reason = reason or self.DEF_REASON
+        await target.kick(reason=reason)
 
-	@commands.command()
-	@commands.has_permissions(kick_members=True)
-	@commands.bot_has_permissions(kick_members=True)
-	async def kick(self, ctx: commands.Context, target: Member, *, reason:Optional[str]) -> None:
-		"""Kicks the given target for a reason"""
-		reason = reason or self.DEF_REASON
-		await target.kick(reason=reason)
-	
-	@commands.command() 
-	@commands.has_permissions(ban_members=True)
-	@commands.bot_has_permissions(ban_members=True)
-	async def unban(self, ctx: commands.Context, target: int, *, reason: Optional[str]) -> None:
-		"""Unbans the given target"""
-		reason = reason or self.DEF_REASON
-		await ctx.guild.unban(Object(id=target), reason=reason)
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    @commands.bot_has_permissions(ban_members=True)
+    async def unban(self, ctx: commands.Context, target: int, *, reason: Optional[str]) -> None:
+        """Unbans the given target"""
+        reason = reason or self.DEF_REASON
+        await ctx.guild.unban(Object(id=target), reason=reason)
 
-	@commands.command()
-	@commands.has_guild_permissions(mute_members=True)
-	@commands.bot_has_guild_permissions(mute_members=True)
-	async def mute(self, ctx: commands.Context, target: Member, *, reason: Optional[str]) -> None:
-		"""Mutes the given target with a reason"""
-		reason = reason or self.DEF_REASON
-		await target.edit(mute=True, reason=reason)
+    @commands.command()
+    @commands.has_guild_permissions(mute_members=True)
+    @commands.bot_has_guild_permissions(mute_members=True)
+    async def mute(self, ctx: commands.Context, target: Member, *, reason: Optional[str]) -> None:
+        """Mutes the given target with a reason"""
+        reason = reason or self.DEF_REASON
+        await target.edit(mute=True, reason=reason)
 
-	@commands.command()
-	@commands.has_guild_permissions(mute_members=True)
-	@commands.bot_has_guild_permissions(mute_members=True)
-	async def unmute(self, ctx: commands.Context, target: Member, *, reason: Optional[str]) -> None:
-		reason = reason or self.DEF_REASON
-		await target.edit(mute=False, reason=reason)
+    @commands.command()
+    @commands.has_guild_permissions(mute_members=True)
+    @commands.bot_has_guild_permissions(mute_members=True)
+    async def unmute(self, ctx: commands.Context, target: Member, *, reason: Optional[str]) -> None:
+        reason = reason or self.DEF_REASON
+        await target.edit(mute=False, reason=reason)
+
 
 def setup(bot):
-	bot.add_cog(Moderation(bot))
- 
+    """ Cog entrypoint. """
+    bot.add_cog(Moderation(bot))

cb52d32758642597770a3553ef38c7077fcddc0a
Revan <AlexNorgaard1170@gmail.com>
2020-05-17 18:43:07 +0100

    Config updates. Now calling app info to populate owner IDs. Also updated template with false data

--- config-template.py
+++ config-template.py
@@ -1,4 +1,18 @@
-BOT_TOKEN = "TOKEN HERE WOOOO"
-WEBHOOK = (711260277192575042,
-           "rJASHh1omoWfqTmq6ou9oKVATG_gNIZ6ZC1G2FMRDXPPcDa9")
-OWNER_IDS = [123]
+# ? Bot token
+BOT_TOKEN = "TOKEN HERE"
+
+# ? Webhook tuple. Channel ID as int then secret as a string.
+WEBHOOK = (710260377199575072,
+           "rJASHh1omoW7F8AAPHqh2gGTG_gNIZ6ZC1G2FMRDXPPcDa99")
+
+
+# ? openweathermap
+WEATHER_TOKEN = 'e7b0b8448e85ecb4c01e13'
+
+# ? saucenao
+SAUCENAO_TOKEN = 'b70b01063a9b53deb0c2a35b2e'
+
+# ? google api keys
+GOOGLE_TOKENS = ['AIzaSy4DsNdB5pcDmGiUQdHvA',
+                 'AIzaSyOL3R6QbjILaFoPtObbE',
+                 'AIzaSyBJrcUUdcgisxj_j7vzc']

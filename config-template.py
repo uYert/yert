c57722c24f1533236a765ad408b8e2bd2cbf1a6c
Revan <AlexNorgaard1170@gmail.com>
2020-05-19 05:29:09 +0100

    new config key

--- config-template.py
+++ config-template.py
@@ -4,7 +4,7 @@ BOT_TOKEN = "TOKEN HERE"
 PREFIX = "yoink "
 PSQL_DETAILS = "postgres://<user>:<password>@<IP/Addr>:<Port>/<database_name>"
 
-# ? Webhook tuple. Channel ID as int then secret as a string.
+# ? Webhook tuple. Webhook ID as int then secret as a string.
 WEBHOOK = (710260377199575072,
            "rJASHh1omoW7F8AAPHqh2gGTG_gNIZ6ZC1G2FMRDXPPcDa99")
 
@@ -19,3 +19,5 @@ SAUCENAO_TOKEN = 'b70b01063a9b53deb0c2a35b2e'
 GOOGLE_TOKENS = ['AIzaSy4DsNdB5pcDmGiUQdHvA',
                  'AIzaSyOL3R6QbjILaFoPtObbE',
                  'AIzaSyBJrcUUdcgisxj_j7vzc']
+
+TRAVITIA_TOKEN = "abcdef"

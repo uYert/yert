7c36baa0328c9102b8494724ee294551fb2cd5fc
sudosnok <subsonic96@gmail.com>
2020-05-25 23:01:05 +0100

    tidied up the error handler since we dont mind it being sent to the webhook

--- cogs/events.py
+++ cogs/events.py
@@ -39,24 +39,29 @@ class Events(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.webhook = self._webhook()
+        self.webhook = self._webhook
 
         self.ignored = [commands.CommandNotFound, ]
 
+    @property
     def _webhook(self) -> discord.Webhook:
         wh_id, wh_token = config.WEBHOOK
         hook = discord.Webhook.partial(
             id=wh_id, token=wh_token, adapter=discord.AsyncWebhookAdapter(self.bot.session))
         return hook
 
-    async def any_role_converter(self, ctx: NewCtx,
-                                 args: typing.List[typing.Union[int, str]]
-                                 ) -> typing.List[str]:
-        """ Converts to a role object. """
-        for idx, item in enumerate(args):
-            if isinstance(item, int):
-                args[idx] = await commands.RoleConverter().convert(ctx, str(item))
-        return args
+    def tracy_beaker_fmt(self, error: Exception) -> typing.Tuple[str, str]:
+        full_exc = traceback.format_exception(type(error), error, error.__traceback__)
+        short_exc = full_exc[-1]
+        full_exc = [line.replace('/home/moogs', '', 1) for line in full_exc]
+        full_exc = [line.replace('C:\\Users\\aaron', '', 1) for line in full_exc]
+        output = '\n'.join(full_exc)
+        idx = 0
+        while len(output) >= 1990:
+            idx -= 1
+            output = '\n'.join(full_exc[:idx])
+        output = f"```{output}```"
+        return short_exc, output
 
     @commands.group(invoke_without_command=True, name="ignored")
     @commands.is_owner()
@@ -120,115 +125,16 @@ class Events(commands.Cog):
 
         error = getattr(error, 'original', error)
 
-        if isinstance(error, commands.MissingPermissions):
-            message = (f"{ctx.author.mention}, you're missing the "
-                       f"following persissions to use that command; {error.missing_perms}")
-            return await ctx.webhook_send(message, webhook=self.webhook)
-
-        if isinstance(error, commands.BotMissingPermissions):
-            message = (f"{ctx.author.mention}, I'm missing permissions "
-                       f"for that command; {error.missing_perms}")
-            return await ctx.webhook_send(message, webhook=self.webhook)
-
         if isinstance(error, commands.CommandOnCooldown):
-            message = (f"{ctx.author.mention}, that command is on cooldown"
-                       f" for another {error.retry_after}s.")
-            
             if await self.bot.is_owner(ctx.author):
                 return await ctx.reinvoke()
-            
-            return await ctx.webhook_send(message, webhook=self.webhook, skip_ctx=True)
 
-        if isinstance(error, commands.MissingRequiredArgument):
-            message = (f"{ctx.author.mention}, you were missing at least one"
-                       f" argument from that command; {error.param}")
-            return await ctx.webhook_send(message, webhook=self.webhook)
+        short, full = self.tracy_beaker_fmt(error)
 
-        if isinstance(error, commands.BadArgument):
-            bad_argument = list(ctx.command.clean_params)[len(ctx.args[2:])]
-            bad_typehint = signature(
-                ctx.command.callback).parameters[bad_argument].annotation
-            message = "{0.display_name}, argument {1} was expecting {2}".format(
-                ctx.author, bad_argument, bad_typehint)
-            await ctx.send(message)
-            tracy_beaker = traceback.format_exception(
-                type(error), error, error.__traceback__
-            )
-            message = "\n".join(tracy_beaker)
-            idx = 0
-            while len(message) >= 1990:
-                idx -= 1
-                message = "\n".join(tracy_beaker[:idx])
-            message = "```{0}```".format(message)
-            return await self.webhook.send(message)
+        await ctx.send(short)
+        await self.webhook.send(full)
 
 
-        if isinstance(error, commands.BadUnionArgument):
-            bad_argument = error.param
-            bad_typehints = error.converters
-            message = "{0.mention}, argument {1} was expecting {2}".format(
-                ctx.author, bad_argument, *bad_typehints)
-            return await ctx.webhook_send(message, webhook=self.webhook)
-
-        if isinstance(error, commands.MaxConcurrencyReached):
-            max_conc = error.number
-            message = (f"{ctx.author.mention}, {ctx.command.name} has reached"
-                       f" maximum concurrent uses at {max_conc}")
-            return await ctx.webhook_send(message, webhook=self.webhook)
-
-        if isinstance(error, commands.MissingRole):
-            if isinstance(error.missing_role, int):
-                role_name = commands.RoleConverter().convert(ctx, str(error.missing_role))
-            else:
-                role_name = error.missing_role
-            message = "{0.mention}, you're missing the role {1}".format(
-                ctx.author, role_name)
-            return await ctx.webhook_send(message, webhook=self.webhook)
-
-        if isinstance(error, commands.BotMissingRole):
-            if isinstance(error.missing_role, int):
-                role_name = commands.RoleConverter().convert(ctx, str(error.missing_role))
-            else:
-                role_name = error.missing_role
-            message = "{0.mention}, I'm missing the role {1}".format(
-                ctx.author, role_name)
-            return await ctx.webhook_send(message, webhook=self.webhook)
-
-        if isinstance(error, commands.MissingAnyRole):
-            role_names = await self.any_role_converter(ctx, error.missing_roles)
-            message = "{0.mention}, you're missing these roles for that command {1}".format(
-                ctx.author, ', '.join(role_names))
-            return await ctx.webhook_send(message, webhook=self.webhook)
-
-        if isinstance(error, commands.BotMissingAnyRole):
-            role_names = await self.any_role_converter(ctx, error.missing_roles)
-            message = "{0.mention}, I'm missing these roles for {1}".format(
-                ctx.author, ', '.join(role_names))
-            return await ctx.webhook_send(message, webhook=self.webhook)
-
-        if isinstance(error, commands.ConversionError):
-            converter_name = error.converter.__name__
-            cause = error.__cause__
-            message = "{0.mention}, a conversion error occured with the {1} due to {2}".format(
-                ctx.author, converter_name, cause)
-            return await ctx.webhook_send(message, webhook=self.webhook)
-
-        if isinstance(error, AssertionError):
-            message = "AssertionError : {0}".format(error.args[0])
-            return await ctx.webhook_send(message, webhook=self.webhook, skip_ctx=True)
-
-        else:
-            tracy_beaker = traceback.format_exception(
-                type(error), error, error.__traceback__)
-            tracy_beaker = [bork.replace('/home/moogs', '', 1) for bork in tracy_beaker]
-            tracy_beaker = [bork.replace(r'C:\\Users\\aaron', '', 1) for bork in tracy_beaker]
-            message = "\n".join(tracy_beaker)
-            idx = 0
-            while len(message) >= 1990:
-                idx -= 1
-                message = "\n".join(tracy_beaker[:idx])
-            message = "```{0}```".format(message)
-            return await ctx.webhook_send(message, webhook=self.webhook)
 
     @commands.Cog.listener()
     async def on_command_completion(self, ctx: NewCtx):
